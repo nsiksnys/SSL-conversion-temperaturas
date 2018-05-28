95f2767c3c5873aeacbccb4069413e1e40d89f5f
alastiesas <agustin.lastiesas@gmail.com>
2018-05-28 15:21:32 -0300

    ultimo

--- 02-Interfaces/TablasDeConversion.c
+++ 02-Interfaces/TablasDeConversion.c
@@ -1,20 +1,65 @@
 #include <stdio.h>
 #include "Conversion.h"
 
+void PrintTablas (int MIN, int MAX, int ESC);
+void PrintTablaFahrenheit(int MIN, int MAX, int ESC);
+void PrintTablaCelcius(int MIN, int MAX, int c);
+void PrintFilas (int MIN,int MAX,int ESC,int a,int b,double(*f)(int));
+void PrintFila (int a,int b,int x, double(*f)(int));
+
+
 int main(void) {
-	const int	MIN = 0, // límite mínimo tabla
-				MAX = 300, // límite máximo
+	const int	MIN = 0, // limite minimo tabla
+				MAX = 300, // limite maximo tabla
 				ESC = 20; // escala salto
 	
-	int fahr, cel;
-	
-	printf("Fahrenheit a Celsius\n");
-	for (fahr = MIN; fahr <= MAX; fahr = fahr + ESC){
-		printf("%3d F° =%6.1f C°\n", fahr, Celsius(fahr));
-	}
-	
+	PrintTablas(MIN,MAX,ESC);
+
+	return 0;
+}
+
+
+
+void PrintTablas (int MIN, int MAX, int ESC){
+
+    printf("Fahrenheit a Celsius\n");
+	PrintTablaFahrenheit (MIN,MAX,ESC);
+
 	printf("\n\nCelsius a Fahrenheit\n");
-	for (cel = MIN; cel <= MAX; cel = cel + ESC){
-		printf("%3d C° =%6.1f F°\n", cel, Fahrenheit(cel));
+	PrintTablaCelcius (MIN,MAX,ESC);
+
+}
+
+void PrintTablaFahrenheit(int MIN, int MAX, int ESC){
+
+	
+	int f=70;
+	int c=67; 
+
+	PrintFilas(MIN,MAX,ESC,c,f,&Celsius);
+}
+
+void PrintTablaCelcius (int MIN, int MAX, int ESC){
+
+	
+	int f=70;
+	int c=67;
+
+	PrintFilas (MIN,MAX,ESC,f,c,&Fahrenheit);
+}
+
+void PrintFilas (int MIN,int MAX,int ESC,int a,int b,double(*f)(int)){
+
+	int x;
+
+	for (x = MIN; x <= MAX; x = x + ESC){
+		PrintFila(a,b,x,&(*f));
 	}
 }
+
+
+void PrintFila(int a,int b,int x, double(*f)(int)){
+
+	printf("%3d %cÂ° =%6.1f %cÂ°\n", x, a, (*f)(x), b);
+}
+
